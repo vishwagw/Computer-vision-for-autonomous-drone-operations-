b391b73c6d002eb18344ca3c589c6ed9d0d6da26
Vishwa GW (Randy) <136871744+vishwagw@users.noreply.github.com>
2025-04-12 06:26:17 +0530

    Update acc_detector.py

--- Accident detection in real time using CV/acc_detector.py
+++ Accident detection in real time using CV/acc_detector.py
@@ -1,1 +1,6 @@
 # This program can be used for both real time and recorded data (for testing) based detections.
+# program codbase:
+import cv2
+import numpy as np
+import time
+from collections import deque
